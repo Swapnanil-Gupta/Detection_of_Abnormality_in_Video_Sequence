d82c96fb7b7a008e39e7ba0b7beb677f2327d7db
Swapnanil Gupta <gupta.swapnanil@gmail.com>
2019-01-25 16:32:32 +0530

    added left_track, right_track and event classification(normal/abnormal)

---
+++ left_track_normal.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture("video/capture_left.mp4")
+_, frame = cap.read()
+old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+lk_params = dict(winSize=(15, 15),
+                 maxLevel=4,
+                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+# from first frame
+p1 = (354 + 476)//2
+p2 = (148 + 251)//2
+center = (p1, p2)
+
+old_points = np.array([[p1, p2]], dtype=np.float32)
+
+#cv2.circle(frame, center, 1, (0, 0, 255), 1)
+while True:
+    # read recent frame
+    ret, frame = cap.read()
+    # check empty frame
+    if not ret:
+        break
+    # convert to grayscale
+    # cv2.rectangle(frame, (267, 136), (402, 247), (0, 0, 255), 2)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    new_points, status, error = cv2.calcOpticalFlowPyrLK(old_gray, gray_frame, old_points, None, **lk_params)
+    old_gray = gray_frame.copy()
+    old_points = new_points
+    x, y = new_points.ravel()
+
+    #cv2.circle(frame, (x, y), 10, (0, 0, 255), -1)
+    point_x1 = int(x + 122//2)
+    #print(point_x1)
+    point_y1 = int(y + 103//2)
+    #print(point_y1)
+    point_x2 = int(x - 122//2)
+    #print(point_x2)
+    point_y2 = int(y - 103//2)
+    #print(point_y2)
+    cv2.rectangle(frame, (point_x1, point_y1), (point_x2, point_y2), (0, 0, 255), 2)
+
+    if x <= (p1 + 0.25):
+        print('left')
+        cv2.putText(frame, 'NORMAL', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), 2)
+
+    p1, p2 = x, y
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+# release the capture and destroy the windows
+cap.release()
+cv2.destroyAllWindows()
---
+++ nomral_abnormal.py
@@ -0,0 +1,2 @@
+exec(open('left_track_normal.py').read())
+exec(open('right_track_abnormal.py').read())
